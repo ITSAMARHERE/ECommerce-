12fff5bfc422b46ab1cf581c0bada34c6ca89c4c
Amar pal <amarpal.i.a.s@gmail.com>
2025-04-07 15:59:04 +0530

    update init

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -112,7 +112,58 @@ const fetchCartItems = async (req, res) => {
 
 const updateCartItemQty = async (req, res) => {
     try {
+        const { userId, productId, quantity } = req.body;
 
+        if (!userId || !productId || quantity <= 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Inavalid data provides!"
+            })
+        }
+
+        const cart = await Cart.findOne({userId})
+        if(!cart){
+            return res.status(404).json({
+                success : false,
+                message: "Cart not found!"
+            });
+        }
+
+
+        const findCurrentProductIndex = cart.items.findIndex(item=> item.productId.toString() === productId);
+
+        if(findCurrentProductIndex === -1){
+            return res.status(404).json({
+                success : false,
+                message : "Cart item not present!"
+            })
+        }
+
+        cart.items[findCurrentProductIndex].quantity = quantity 
+        await cart.save();
+
+        await cart.populate({
+            path : "items.productId",
+            select : "image title price salePrice", 
+        })
+
+        const populateCartItems = cart.items.map(item => ({
+            productId: item.productId ? item.productId._id : null,
+            image: item.image ? item.productId.image : null,
+            title: item.title ? item.productId.title : "Product not found",
+            price: item.price ? item.productId.price : null,
+            salePrice: item.salePrice ? item.productId.salePrice : null,
+            quantity: item.quantity
+        }));
+
+        res.status(200).json({
+            success : true,
+            data: {
+                ...cart.doc,
+                items : populateCartItems,
+            },
+        });
+        
     } catch (error) {
         console.log(error);
         res.status(500).json({
