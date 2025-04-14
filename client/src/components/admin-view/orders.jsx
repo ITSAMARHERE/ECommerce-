d93e8f46bff1e83dec500827f0aea72438c2684f
Amar pal <amarpal.i.a.s@gmail.com>
2025-04-14 17:11:49 +0530

    order admin init

--- client/src/components/admin-view/orders.jsx
+++ client/src/components/admin-view/orders.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
 import AdminOrderDetailsView from "./order-details";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllOrdersForAdmin, getOrderDetailsForAdmin, resetOrderDetails } from "@/store/admin/order-slice";
+import { Badge } from "../ui/badge";
 
 function AdminOrdersView() {
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const {orderList, orderDetails} = useSelector(state=>state.adminOrder);
+  const dispatch = useDispatch();
+
+  function handleFetchOrderDetails(getId){
+    dispatch(getOrderDetailsForAdmin(getId))
+  }
+
+  useEffect(()=>{
+    dispatch(getAllOrdersForAdmin())
+  },[dispatch])
+
+  console.log(orderDetails, "orderDetails");
+
+  useEffect(()=>{
+    if(orderDetails !== null) setOpenDetailsDialog(true)
+  },[orderDetails])
 
   return (
     <Card className="w-full border border-gray-200 shadow-sm">
@@ -27,33 +46,58 @@ function AdminOrdersView() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell>123456</TableCell>
-              <TableCell>27/06/2025</TableCell>
-              <TableCell>
-                <span className="px-2 py-1 rounded-md bg-yellow-100 text-yellow-800 text-sm font-medium">
-                  In Process
-                </span>
-              </TableCell>
-              <TableCell>$1000</TableCell>
-              <TableCell className="text-right">
-                <Button size="sm" onClick={() => setOpenDetailsDialog(true)} className="cursor-pointer">
-                  View Details
-                </Button>
-              </TableCell>
-            </TableRow>
+          {orderList && orderList.length > 0
+              ? orderList.map((orderItem) => (
+                  <TableRow key={orderItem._id} className="border-b border-gray-300 hover:bg-gray-50">
+                    <TableCell className="border-r border-gray-300 px-4 py-2">{orderItem?._id}</TableCell>
+                    <TableCell className="border-r border-gray-300 px-4 py-2">{orderItem?.orderDate.split("T")[0]}</TableCell>
+                    <TableCell className="border-r border-gray-300 px-4 py-2">
+                      <Badge
+                        className={`py-1 px-3 capitalize text-white ${
+                          orderItem?.orderStatus === "confirmed"
+                            ? "bg-green-500"
+                            : orderItem?.orderStatus === "rejected"
+                            ? "bg-red-600"
+                            : "bg-black"
+                        }`}
+                      >
+                        {orderItem?.orderStatus}
+                      </Badge>
+                    </TableCell>
+                    <TableCell className="border-r border-gray-300 px-4 py-2">${orderItem?.totalAmount}</TableCell>
+                    <TableCell className="px-4 py-2">
+                      <Dialog
+                        open={openDetailsDialog}
+                        onOpenChange={() => {
+                          setOpenDetailsDialog(false);
+                          dispatch(resetOrderDetails());
+                        }}
+                      >
+                        <Button
+                          onClick={() => {
+                            setOpenDetailsDialog(true);
+                            handleFetchOrderDetails(orderItem?._id);
+                          }}
+                          variant="outline"
+                          className="text-sm cursor-pointer"
+                        >
+                          View Details
+                        </Button>
+                        <AdminOrderDetailsView orderDetails={orderDetails} />
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : (
+                <TableRow>
+                  <TableCell colSpan={5} className="text-center py-6 text-gray-500">
+                    No orders found.
+                  </TableCell>
+                </TableRow>
+              )}
           </TableBody>
         </Table>
       </CardContent>
-
-      <Dialog open={openDetailsDialog} onOpenChange={setOpenDetailsDialog}>
-        <DialogContent className="sm:max-w-[600px]">
-          <DialogHeader>
-            <DialogTitle>Order Details</DialogTitle>
-          </DialogHeader>
-          <AdminOrderDetailsView />
-        </DialogContent>
-      </Dialog>
     </Card>
   );
 }
